3bc810c596433dcb19e905003229472b09da4a5b
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-04-17 15:45:22 -0400

    Update First_Unique_Character_in_a_String.py

--- Hash Table/First_Unique_Character_in_a_String.py
+++ Hash Table/First_Unique_Character_in_a_String.py
@@ -1,5 +1,7 @@
 # Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
+# Approach 1:
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         res = {}
@@ -11,4 +13,15 @@ class Solution:
         for char, [count, index] in res.items():
             if count == 1:
                 return index
-        return -1       
+        return -1   
+
+#Approach 2
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        c = Counter(s)
+        for i in range(len(s)):
+            if c[s[i]] == 1:
+                return i
+        return -1
+            
