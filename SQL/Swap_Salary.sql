940682a8538939b7ae2f00cd8ddaa1e079953132
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-04-27 21:34:37 -0400

    Create Swap_Salary.sql

---
+++ SQL/Swap_Salary.sql
@@ -0,0 +1,22 @@
+-- Table: Salary
+
+-- +-------------+----------+
+-- | Column Name | Type     |
+-- +-------------+----------+
+-- | id          | int      |
+-- | name        | varchar  |
+-- | sex         | ENUM     |
+-- | salary      | int      |
+-- +-------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- The sex column is ENUM (category) value of type ('m', 'f').
+-- The table contains information about an employee.
+ 
+
+-- Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+-- Note that you must write a single update statement, do not write any select statement for this problem.
+
+-- # Write your MySQL query statement below
+UPDATE Salary
+SET sex = (CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END)
