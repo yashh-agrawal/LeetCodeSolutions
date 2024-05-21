48880778b88dabae9312bf9409f239245dd0dae8
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-05-21 15:14:02 -0400

    Create Find_Followers_Count.sql

---
+++ SQL/Find_Followers_Count.sql
@@ -0,0 +1,23 @@
+-- Table: Followers
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | user_id     | int  |
+-- | follower_id | int  |
+-- +-------------+------+
+-- (user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+-- This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+ 
+
+-- Write a solution that will, for each user, return the number of followers.
+
+-- Return the result table ordered by user_id in ascending order.
+
+-- The result format is in the following example.
+
+-- # Write your MySQL query statement below
+
+SELECT user_id, COUNT(*) as followers_count from Followers
+GROUP BY user_id
+ORDER BY user_id;
