f9ac7848409411174e5973443d9019c3ecfc0698
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-06-03 15:25:13 -0400

    Create Customers_Who_Visited_but_Did_Not_Make_Any_Transactions.sql

---
+++ SQL/Customers_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,37 @@
+-- Table: Visits
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+-- visit_id is the column with unique values for this table.
+-- This table contains information about the customers who visited the mall.
+ 
+
+-- Table: Transactions
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+-- transaction_id is column with unique values for this table.
+-- This table contains information about the transactions made during the visit_id.
+ 
+
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+-- Return the result table sorted in any order.
+
+-- The result format is in the following example.
+
+-- # Write your MySQL query statement below
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
