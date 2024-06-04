13331099bed09c42f2aee0d06bb4080e76757a86
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-06-04 15:27:57 -0400

    Create K-th_Symbol_in_Grammar.py

---
+++ Recursion/K-th_Symbol_in_Grammar.py
@@ -0,0 +1,20 @@
+# We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. 
+# Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
+
+# For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
+# Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table of n rows.
+
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        return self.recursion(n,k)
+    def recursion(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        total_elements = 2 ** (n-1)
+        half_elements = total_elements // 2
+
+        if k > half_elements:
+            return 1 - self.recursion(n, k - half_elements)
+        
+        return self.recursion(n-1,k)
