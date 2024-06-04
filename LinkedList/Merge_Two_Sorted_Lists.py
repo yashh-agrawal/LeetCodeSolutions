e18af5206c92b67cb10a782bdf4c26b431efec0c
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-06-04 14:44:52 -0400

    Update Merge_Two_Sorted_Lists.py

--- LinkedList/Merge_Two_Sorted_Lists.py
+++ LinkedList/Merge_Two_Sorted_Lists.py
@@ -25,3 +25,30 @@ class Solution:
         cur.next = list1 or list2
         return dummy.next
         
+#Approach 2 (Recursion)
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+
+        def merge(dummy,node1,node2):
+
+            if node1 is None and node2 is None:
+                return
+            if node1 is None and node2 is not None:
+                return node2
+            if node1 is not None and node2 is None:
+                return node1
+            if node1.val <= node2.val:
+                dummy = node1
+                dummy.next = merge(dummy,node1.next,node2)
+            if node1.val > node2.val:
+                dummy = node2
+                dummy.next = merge(dummy,node1,node2.next)
+            return dummy
+        return merge(dummy,list1,list2)        
