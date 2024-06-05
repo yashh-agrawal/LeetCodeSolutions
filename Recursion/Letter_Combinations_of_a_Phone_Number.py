9f4a4a2c24054c22c8faa392a1c931f93d08bda8
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-06-05 14:58:41 -0400

    Create Letter_Combinations_of_a_Phone_Number.py

---
+++ Recursion/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,32 @@
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+# Return the answer in any order.
+
+# A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        if not digits:
+            return []
+        digit_to_letters = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+
+        def backtrack(combination, next_digits):
+            if not next_digits:
+                res.append(combination)
+                return
+            
+            for letter in digit_to_letters[next_digits[0]]:
+                backtrack(combination + letter, next_digits[1:])
+        
+        backtrack("", digits)
+        return res
+            
