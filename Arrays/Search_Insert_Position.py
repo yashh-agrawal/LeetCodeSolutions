af6e4127d678f1409d3d12759b10a1daf8f424f0
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-06-19 15:25:06 -0400

    Update Search_Insert_Position.py

--- Arrays/Search_Insert_Position.py
+++ Arrays/Search_Insert_Position.py
@@ -11,3 +11,20 @@ class Solution:
             if nums[i] > target or nums[i] == target:
                 return i
         return len(nums)
+
+#Approach II (Binary Search)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid -1
+            else:
+                left = mid + 1
+        return left 
