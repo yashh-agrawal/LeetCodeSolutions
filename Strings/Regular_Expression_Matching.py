062eda79114f0776bbb56ab2add6bb5088879c94
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-06-20 16:11:55 -0400

    Create Regular_Expression_Matching.py

---
+++ Strings/Regular_Expression_Matching.py
@@ -0,0 +1,27 @@
+# Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+# '.' Matches any single character.​​​​
+# '*' Matches zero or more of the preceding element.
+# The matching should cover the entire input string (not partial).
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        dp[0][0] = True
+
+        for j in range(1, n+1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-2]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if p[j-1] == '.' or p[j-1] == s[i-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-2]
+                    if p[j-2] == '.' or p[j-2] == s[i-1]:
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
+        
+        return dp[m][n]
