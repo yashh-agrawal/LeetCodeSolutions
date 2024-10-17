769d68a2c9b4ab549ff0665d350da6f368bcde5e
yashagrawal13072000 <yashagrawal13072000@gmail.com>
2024-10-17 13:54:00 -0400

    Create Decode_String.py

---
+++ Stacks/Decode_String.py
@@ -0,0 +1,29 @@
+# Given an encoded string, return its decoded string.
+
+# The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+# You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+# The test cases are generated so that the length of the output will never exceed 105.
+
+ class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char != "]":
+                stack.append(char)
+            
+            else:
+                curr_str = ""
+                while stack[-1] != "[":
+                    curr_str = stack.pop() + curr_str
+                stack.pop()
+
+                curr_num=""
+                while stack and stack[-1].isdigit():
+                    curr_num = stack.pop() + curr_num
+                curr_str = int(curr_num) * curr_str
+                stack.append(curr_str)
+        
+        return "".join(stack)
