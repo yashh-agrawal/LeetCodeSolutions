b63807e7a52e14c68ac5ebc7deb27002329d63c8
yashagrawal13072000 <yashagrawal13072000@gmail.com>
2024-10-21 16:10:51 -0400

    Create Search_for_a_Range.py

---
+++ Binary Search/Search_for_a_Range.py
@@ -0,0 +1,35 @@
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findLeft(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+        
+        def findRight(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return right
+        
+        leftIndex = findLeft(nums, target)
+        rightIndex = findRight(nums, target)
+        
+        if leftIndex <= rightIndex and rightIndex < len(nums) and nums[leftIndex] == target and nums[rightIndex] == target:
+            return [leftIndex, rightIndex]
+        else:
+            return [-1, -1]
