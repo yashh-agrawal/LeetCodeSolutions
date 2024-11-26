42b485747d27b8ddb84b5be16c63bff4845689ad
yashagrawal13072000 <yashagrawal13072000@gmail.com>
2024-11-26 14:11:52 -0500

    Create Merge_Strings_Alternately.py

---
+++ Strings/Merge_Strings_Alternately.py
@@ -0,0 +1,15 @@
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+# Return the merged string.
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = []
+
+        for a, b in zip(word1, word2):
+            merged.append(a + b)
+        
+        merged.append(word1[len(word2):])
+        merged.append(word2[len(word1):])
+
+        return "".join(merged)
